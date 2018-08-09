5d1ef594457e250744b1579e5cdb2026d18efdeb
zchwang <zchwang@chinaums.com>
2018-08-09 19:30:24 +0800

    web后台，constant字段应该怎么放

---
+++ rest-demo/src/main/java/line/entertains/boot/cnst/ErrorConstants.java
@@ -0,0 +1,13 @@
+package line.entertains.boot.cnst;
+
+public class ErrorConstants {
+	public static class ERR_CODE {
+		public static final String SUCC = "0000";
+		public static final String FATAL = "9999";
+	}
+	
+	public static class ERR_INFO {
+		public static final String SUCC = "成功!";
+		public static final String FATAL = "系统内部错误!";
+	}
+}
---
+++ rest-demo/src/test/java/line/entertains/boot/CnstTest.java
@@ -0,0 +1,15 @@
+package line.entertains.boot;
+
+import line.entertains.boot.cnst.ErrorConstants.ERR_CODE;
+import line.entertains.boot.cnst.ErrorConstants.ERR_INFO;
+
+public class CnstTest {
+
+	public static void main(String[] args) {
+		String errCode = ERR_CODE.SUCC;
+		String errInfo = ERR_INFO.SUCC;
+		
+		System.out.println(errCode);
+		System.out.println(errInfo);
+	}
+}
