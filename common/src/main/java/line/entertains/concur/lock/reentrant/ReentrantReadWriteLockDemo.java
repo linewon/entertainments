364f133ec8012f4abdec718dd45796121451cced
zchwang <zchwang@chinaums.com>
2018-12-28 16:56:42 +0800

    reentrantReadWriteLock: 锁降级

--- common/src/main/java/line/entertains/concur/lock/reentrant/ReentrantReadWriteLockDemo.java
+++ common/src/main/java/line/entertains/concur/lock/reentrant/ReentrantReadWriteLockDemo.java
@@ -23,7 +23,9 @@ public class ReentrantReadWriteLockDemo {
 		ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 		StringBuilder sb = new StringBuilder("WELCOME!\n");
 //		test1(sb, lock);
-		test2(sb, lock);
+//		test2(sb, lock);
+		test3(sb, lock);
+		System.out.println("\n********** Finally:\n" + sb.toString());
 	}
 	/**
 	 * 简单读写锁测试
@@ -44,14 +46,17 @@ public class ReentrantReadWriteLockDemo {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.println("\n********** Finally:\n" + sb.toString());
 	}
 	/**
 	 * 随机生成读线程或写线程
+	 * ATTENTION: if w1,r1,r2,r3,w2,r4,r5
+	 * when the first read acquirdShared(),
+	 * the followed r2,r3 will acuiredShared() too, but not the r4,r5.
+	 * last two read will acuiredShared() after w2.unlock()!
 	 */
 	public static void test2(StringBuilder sb, ReentrantReadWriteLock lock) {
 		Random ran = new Random();
-		final int CNT = 5; // 小一点为了debug，大一点为了print
+		final int CNT = 7; // 小一点为了debug，大一点为了print
 		CountDownLatch latch = new CountDownLatch(CNT);
 		for (int i = 0; i < CNT; i++) {
 			My my = null;
@@ -68,9 +73,74 @@ public class ReentrantReadWriteLockDemo {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.println("\n********** Finally:\n" + sb.toString());
+	}
+	/**
+	 * 写锁降级
+	 */
+	public static void test3(StringBuilder sb, ReentrantReadWriteLock lock) {
+		lock = new ReentrantReadWriteLock(true); // 公平锁情况下的锁降级
+		
+		CountDownLatch latch = new CountDownLatch(2);
+		MyRead read = new MyRead(sb, lock, latch);
+		My write = new My(sb, lock, latch) { // 定义一个降级的写锁
+			@Override
+			public void run() {
+				lock.writeLock().lock();
+				doWrite();
+				lock.readLock().lock(); // 降级：从写锁降为读锁
+				lock.writeLock().unlock(); // 同时释放写锁
+				// 从打印结果可以看到，writeLock.unlock之后，另一个线程立刻就能获取读锁了。
+				doRead();
+				SleepUtils.sleep(5000);
+				lock.readLock().unlock();
+				super.latch.countDown();
+			}
+		};
+		
+		new Thread(write).start();
+		SleepUtils.sleep(1000);
+		new Thread(read).start();
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	 * 读线程，打印StringBuilder
+	 */
+	private static class MyRead extends My {
+		public MyRead(StringBuilder sb, ReentrantReadWriteLock lock, CountDownLatch latch) {
+			super(sb, lock, latch);
+		}
+
+		@Override
+		public void run() {
+			super.lock.readLock().lock();
+			doRead();
+			super.lock.readLock().unlock(); // 这里，锁的释放时机。。finally什么什么的
+			super.latch.countDown();
+		}
+	}
+
+	/**
+	 * 写线程，修改StringBuilder
+	 */
+	private static class MyWrite extends My {
+		public MyWrite(StringBuilder sb, ReentrantReadWriteLock lock, CountDownLatch latch) {
+			super(sb, lock, latch);
+		}
+
+		@Override
+		public void run() {
+			super.lock.writeLock().lock();
+			doWrite();
+			super.lock.writeLock().unlock();
+			super.latch.countDown();
+		}
+
+	}
 	private static abstract class My implements Runnable {
 		/** 模拟一个可编辑的文本 */
 		protected StringBuilder sb;
@@ -88,62 +158,31 @@ public class ReentrantReadWriteLockDemo {
 			this.lock = lock;
 			this.latch = latch;
 		}
-
 		protected int randSec() {
 			return random.nextInt(3) + 1;
 		}
-
 		protected String date() {
 			return df.format(new Date()); // 每次都是new，是线程安全的
 		}
-	}
-
-	/**
-	 * 读线程，打印StringBuilder
-	 */
-	private static class MyRead extends My {
-		public MyRead(StringBuilder sb, ReentrantReadWriteLock lock, CountDownLatch latch) {
-			super(sb, lock, latch);
+		protected void doWrite() {
+			String date = date(); // 在这里记录获取写锁的时间
+			int randSec = randSec();
+			SleepUtils.sleep(randSec * 1000);
+			String name = Thread.currentThread().getName();
+			String writing = Joiner.on('+').join(name, randSec);
+			sb.append(writing).append('\n');
+			System.out.println(Joiner.on("  ").join("\nWRITE", date, name, randSec, writing));
 		}
-
-		@Override
-		public void run() {
-			super.lock.readLock().lock();
-			String date = super.date(); // 在这里记录获取读锁的时间
+		protected void doRead() {
+			String date = date(); // 在这里记录获取读锁的时间
 			int randSec = randSec();
 			SleepUtils.sleep(randSec * 1000);
 			/*
 			 * 这个操作，是线程安全的。因为之前获取了读锁，StringBuilder不会发生改变。
 			 */
-			String readString = '\n' + super.sb.toString();
+			String readString = '\n' + sb.toString();
 			String name = Thread.currentThread().getName();
 			System.out.println(Joiner.on("  ").join("\nREAD", date, name, randSec, readString));
-			super.lock.readLock().unlock(); // 这里，锁的释放时机。。finally什么什么的
-			super.latch.countDown();
 		}
-	}
-
-	/**
-	 * 写线程，修改StringBuilder
-	 */
-	private static class MyWrite extends My {
-		public MyWrite(StringBuilder sb, ReentrantReadWriteLock lock, CountDownLatch latch) {
-			super(sb, lock, latch);
-		}
-
-		@Override
-		public void run() {
-			super.lock.writeLock().lock();
-			String date = super.date(); // 在这里记录获取写锁的时间
-			int randSec = randSec();
-			SleepUtils.sleep(randSec * 1000);
-			String name = Thread.currentThread().getName();
-			String writing = Joiner.on('+').join(name, randSec);
-			super.sb.append(writing).append('\n');
-			System.out.println(Joiner.on("  ").join("\nWRITE", date, name, randSec, writing));
-			super.lock.writeLock().unlock();
-			super.latch.countDown();
-		}
-
 	}
 }
